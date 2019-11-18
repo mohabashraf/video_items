2677495b465223e65374d3fb8c6654965d32589b
mohabashraf <mohabashraf466@gmail.com>
2019-11-18 13:28:50 +0200

    initial project

---
+++ src/main/java/read_share_learn/video_items/video_items/VideoItemsApplication.java
@@ -0,0 +1,13 @@
+package read_share_learn.video_items.video_items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/read_share_learn/video_items/video_items/VideoItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package read_share_learn.video_items.video_items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
