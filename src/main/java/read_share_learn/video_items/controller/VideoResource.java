296d5a488e282292d7f762026cc7d95c3733a49e
mohabashraf <mohabashraf466@gmail.com>
2019-11-18 14:23:04 +0200

    add actuator project

--- src/main/java/read_share_learn/video_items/controller/VideoResource.java
+++ src/main/java/read_share_learn/video_items/controller/VideoResource.java
@@ -3,10 +3,12 @@ package read_share_learn.video_items.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Properties;
+
 @RestController
 public class VideoResource {
     @GetMapping("/")
-    public String testGetRest(){
-        return "Test Get";
+    public Properties testGetRest(){
+        return System.getProperties();
     }
 }
